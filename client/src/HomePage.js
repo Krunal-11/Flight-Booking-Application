1613da738afab1ee0d90c66b569bcbe2bb0d5696
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2024-09-24 23:43:05 +0530

    Home page UI updated

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,43 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { Container, TextField, Button, Typography, Grid, Paper, Autocomplete } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  TextField,
+  Autocomplete,
+  Grid,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AirlinesIcon from "@mui/icons-material/Airlines";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import PublicIcon from "@mui/icons-material/Public";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+import { makeStyles } from "@mui/styles";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   root: {
-    marginTop: '5rem',
-    padding: '2rem',
-    backgroundColor: '#e0f7fa',
+    marginTop: "5rem",
+    padding: "2rem",
+    backgroundColor: "#e0f7fa",
   },
   searchContainer: {
-    padding: '2rem',
-    borderRadius: '8px',
-    backgroundColor: '#ffffff',
+    padding: "2rem",
+    borderRadius: "8px",
+    backgroundColor: "#ffffff",
   },
 }));
 
-const HomePage = () => {
+function HomePage() {
   const classes = useStyles();
   const navigate = useNavigate();
   const [locations, setLocations] = useState([]);
   const [searchData, setSearchData] = useState({
-    departureLocation: '',
-    arrivalLocation: '',
-    departureDate: '',
-    numberOfTickets: '',
+    departureLocation: "",
+    arrivalLocation: "",
+    departureDate: "",
+    numberOfTickets: "",
   });
 
   useEffect(() => {
     const fetchLocations = async () => {
       try {
-        const response = await fetch('http://localhost:8080/api/admin/locations');
+        const response = await fetch(
+          "http://localhost:8080/api/admin/locations"
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setLocations(data); // Assuming this returns an array of location objects
       } catch (error) {
-        console.error('Error fetching locations:', error);
+        console.error("Error fetching locations:", error);
       }
     };
 
@@ -46,90 +64,234 @@ const HomePage = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log('navigating with data :',searchData)
-    navigate('/all-flights', { state: searchData });
+    console.log("navigating with data :", searchData);
+    navigate("/all-flights", { state: searchData });
   };
 
   return (
-    <Container className={classes.root}>
-      <Typography variant="h4" align="center" gutterBottom>
-        Flight Booking Application
-      </Typography>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} md={8}>
-          <Paper className={classes.searchContainer} elevation={3}>
-            <Typography variant="h5" align="center" gutterBottom>
-              Search for Flights
-            </Typography>
-            <form onSubmit={handleSearch}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <Autocomplete
-                    options={locations}
-                    getOptionLabel={(option) => option.city_name}
-                    onChange={(event, newValue) => 
-                      setSearchData({ ...searchData, departureLocation: newValue ? newValue.loc_id : '' }) // Use loc_id instead of city_name
-                    }
-                    renderInput={(params) => (
-                      <TextField {...params} label="Departure Location" variant="outlined" fullWidth />
-                    )}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Autocomplete
-                    options={locations}
-                    getOptionLabel={(option) => option.city_name}
-                    onChange={(event, newValue) => 
-                      setSearchData({ ...searchData, arrivalLocation: newValue ? newValue.loc_id : '' }) // Use loc_id instead of city_name
-                    }
-                    renderInput={(params) => (
-                      <TextField {...params} label="Arrival Location" variant="outlined" fullWidth />
-                    )}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Departure Date"
-                    type="date"
-                    variant="outlined"
-                    fullWidth
-                    InputLabelProps={{ shrink: true }}
-                    value={searchData.departureDate}
-                    onChange={(e) => {
-                      const selectedDate = new Date(e.target.value);
-                      const today = new Date();
-                      today.setHours(0, 0, 0, 0);
+    <Box>
+      {/* App Bar Section */}
+      <AppBar position="static" sx={{ backgroundColor: "#001e3c" }}>
+        <Toolbar>
+          <AirlinesIcon sx={{ fontSize: 35, marginRight: 1 }} />
+          <Typography variant="h6" sx={{ fontSize: 30, flexGrow: 1 }}>
+            AirIndia
+          </Typography>
+          <FavoriteIcon sx={{ marginLeft: 2 }} />
+          <PublicIcon sx={{ marginLeft: 2 }} />
+          <Button color="inherit" sx={{ marginLeft: 2 }}>
+            Log in
+          </Button>
+          <AccountCircleIcon sx={{ marginLeft: 2 }} />
+          <MenuIcon sx={{ marginLeft: 2 }} />
+        </Toolbar>
+      </AppBar>
 
-                      if (selectedDate >= today) {
-                        setSearchData({ ...searchData, departureDate: e.target.value });
-                      } else {
-                        alert("Please select a present or future date.");
-                      }
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Number of Tickets"
-                    type="number"
-                    variant="outlined"
-                    fullWidth
-                    value={searchData.numberOfTickets}
-                    onChange={(e) => setSearchData({ ...searchData, numberOfTickets: e.target.value })}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button variant="contained" color="primary" type="submit" fullWidth>
-                    Search Flights
-                  </Button>
-                </Grid>
+      {/* Search Form Section */}
+      <Box
+        sx={{
+          backgroundColor: "#f0f4f8",
+          padding: 4,
+          textAlign: "center",
+          color: "#001e3c",
+        }}
+      >
+        <Typography variant="h4" sx={{ marginBottom: 2 }}>
+          Millions of cheap flights. One simple search.
+        </Typography>
+
+        <Box
+          sx={{
+            padding: 3,
+            backgroundColor: "#ffffff",
+            borderRadius: 3,
+            boxShadow: "0 8px 20px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <form onSubmit={handleSearch}>
+            <Grid
+              container
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+            >
+              {/* Departure Field */}
+              <Grid item xs={12} sm={3}>
+                <Autocomplete
+                  options={locations}
+                  getOptionLabel={(option) => option.city_name}
+                  onChange={(event, newValue) =>
+                    setSearchData({
+                      ...searchData,
+                      departureLocation: newValue ? newValue.loc_id : "",
+                    })
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Departure Location"
+                      variant="outlined"
+                      fullWidth
+                    />
+                  )}
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                    },
+                  }}
+                />
               </Grid>
-            </form>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+
+              {/* Arrival Field */}
+              <Grid item xs={12} sm={3}>
+                <Autocomplete
+                  options={locations}
+                  getOptionLabel={(option) => option.city_name}
+                  onChange={(event, newValue) =>
+                    setSearchData({
+                      ...searchData,
+                      arrivalLocation: newValue ? newValue.loc_id : "",
+                    })
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Arrival Location"
+                      variant="outlined"
+                      fullWidth
+                    />
+                  )}
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+
+              {/* Departure Date */}
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  label="Departure Date"
+                  type="date"
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{ shrink: true }}
+                  value={searchData.departureDate}
+                  onChange={(e) => {
+                    const selectedDate = new Date(e.target.value);
+                    const today = new Date();
+                    today.setHours(0, 0, 0, 0);
+
+                    if (selectedDate >= today) {
+                      setSearchData({
+                        ...searchData,
+                        departureDate: e.target.value,
+                      });
+                    } else {
+                      alert("Please select a present or future date.");
+                    }
+                  }}
+                  sx={{
+                    backgroundColor: "white",
+                    "& input": {
+                      backgroundColor: "white",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+
+              {/* Travelers Field */}
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  label="Number of Tickets"
+                  type="number"
+                  variant="outlined"
+                  fullWidth
+                  value={searchData.numberOfTickets}
+                  onChange={(e) =>
+                    setSearchData({
+                      ...searchData,
+                      numberOfTickets: e.target.value,
+                    })
+                  }
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+
+              {/* Search Button */}
+              <Grid item xs={12} sm={2}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                  sx={{
+                    backgroundColor: "#ff5a5f",
+                    color: "white",
+                    padding: "15px 0",
+                    borderRadius: 3,
+                    "&:hover": {
+                      backgroundColor: "#e0484d",
+                    },
+                  }}
+                  startIcon={<SearchIcon />}
+                >
+                  Search
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Box>
+    </Box>
   );
-};
+}
 
 export default HomePage;
--- client/src/SearchFlights.jsx
+++ client/src/SearchFlights.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,27 +6,94 @@ import {
   Button,
   Box,
   TextField,
+  Autocomplete,
+  Grid,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import AirlinesIcon from "@mui/icons-material/Airlines";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import PublicIcon from "@mui/icons-material/Public";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+import { makeStyles } from "@mui/styles";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginTop: "5rem",
+    padding: "2rem",
+    backgroundColor: "#e0f7fa",
+  },
+  searchContainer: {
+    padding: "2rem",
+    borderRadius: "8px",
+    backgroundColor: "#ffffff",
+  },
+}));
 
 function SearchPage() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [locations, setLocations] = useState([]);
+  const [searchData, setSearchData] = useState({
+    departureLocation: "",
+    arrivalLocation: "",
+    departureDate: "",
+    numberOfTickets: "",
+  });
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/admin/locations"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setLocations(data); // Assuming this returns an array of location objects
+      } catch (error) {
+        console.error("Error fetching locations:", error);
+      }
+    };
+
+    fetchLocations();
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log("navigating with data :", searchData);
+    navigate("/all-flights", { state: searchData });
+  };
+
   return (
     <Box>
+      {/* App Bar Section */}
       <AppBar position="static" sx={{ backgroundColor: "#001e3c" }}>
         <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <AirlinesIcon sx={{ fontSize: 35, marginRight: 1 }} />
+          <Typography variant="h6" sx={{ fontSize: 30, flexGrow: 1 }}>
             AirIndia
           </Typography>
-          <Button color="inherit">Log in</Button>
+          <FavoriteIcon sx={{ marginLeft: 2 }} />
+          <PublicIcon sx={{ marginLeft: 2 }} />
+          <Button color="inherit" sx={{ marginLeft: 2 }}>
+            Log in
+          </Button>
+          <AccountCircleIcon sx={{ marginLeft: 2 }} />
+          <MenuIcon sx={{ marginLeft: 2 }} />
         </Toolbar>
       </AppBar>
 
+      {/* Search Form Section */}
       <Box
         sx={{
-          backgroundColor: "#001e3c",
-          padding: 3,
+          backgroundColor: "#f0f4f8",
+          padding: 4,
           textAlign: "center",
-          color: "white",
+          color: "#001e3c",
         }}
       >
         <Typography variant="h4" sx={{ marginBottom: 2 }}>
@@ -34,113 +101,194 @@ function SearchPage() {
         </Typography>
 
         <Box
-          container
-          spacing={2}
-          justifyContent="center"
-          sx={{ marginBottom: 3 }}
+          sx={{
+            padding: 3,
+            backgroundColor: "#ffffff",
+            borderRadius: 3,
+            boxShadow: "0 8px 20px rgba(0, 0, 0, 0.1)",
+          }}
         >
-          <Box item xs={12} sm={3}>
-            <TextField
-              fullWidth
-              label="From"
-              variant="outlined"
-              defaultValue="Indira Gandhi International"
-              componentsProps={{
-                sx: {
-                  backgroundColor: "white",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
+          <form onSubmit={handleSearch}>
+            <Grid
+              container
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+            >
+              {/* Departure Field */}
+              <Grid item xs={12} sm={3}>
+                <Autocomplete
+                  options={locations}
+                  getOptionLabel={(option) => option.city_name}
+                  onChange={(event, newValue) =>
+                    setSearchData({
+                      ...searchData,
+                      departureLocation: newValue ? newValue.loc_id : "",
+                    })
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Departure Location"
+                      variant="outlined"
+                      fullWidth
+                    />
+                  )}
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
                     },
-                    "&:hover fieldset": {
-                      borderColor: "white",
-                    },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "white",
-                    },
-                  },
-                },
-              }}
-            />
-          </Box>
+                  }}
+                />
+              </Grid>
 
-          <Box item xs={12} sm={3}>
-            <TextField
-              fullWidth
-              label="To"
-              variant="outlined"
-              defaultValue="Rajiv Gandhi International"
-              componentsProps={{
-                sx: {
-                  backgroundColor: "white",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
+              {/* Arrival Field */}
+              <Grid item xs={12} sm={3}>
+                <Autocomplete
+                  options={locations}
+                  getOptionLabel={(option) => option.city_name}
+                  onChange={(event, newValue) =>
+                    setSearchData({
+                      ...searchData,
+                      arrivalLocation: newValue ? newValue.loc_id : "",
+                    })
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Arrival Location"
+                      variant="outlined"
+                      fullWidth
+                    />
+                  )}
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
                     },
-                    "&:hover fieldset": {
-                      borderColor: "white",
-                    },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "white",
-                    },
-                  },
-                },
-              }}
-            />
-          </Box>
+                  }}
+                />
+              </Grid>
 
-          <Box item xs={12} sm={2}>
-            <TextField
-              fullWidth
-              type="date"
-              label="Depart"
-              componentsLabelProps={{ shrink: true }}
-              defaultValue="2024-09-26"
-              componentsProps={{
-                min: new Date().toISOString().split("T")[0],
-              }}
-              sx={{
-                "& input": {
-                  backgroundColor: "white",
-                },
-              }}
-            />
-          </Box>
+              {/* Departure Date */}
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  label="Departure Date"
+                  type="date"
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{ shrink: true }}
+                  value={searchData.departureDate}
+                  onChange={(e) => {
+                    const selectedDate = new Date(e.target.value);
+                    const today = new Date();
+                    today.setHours(0, 0, 0, 0);
 
-          <Box item xs={12} sm={2}>
-            <TextField
-              fullWidth
-              label="Travellers"
-              variant="outlined"
-              type="number"
-              defaultValue="1"
-              componentsProps={{
-                sx: {
-                  backgroundColor: "white",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
+                    if (selectedDate >= today) {
+                      setSearchData({
+                        ...searchData,
+                        departureDate: e.target.value,
+                      });
+                    } else {
+                      alert("Please select a present or future date.");
+                    }
+                  }}
+                  sx={{
+                    backgroundColor: "white",
+                    "& input": {
+                      backgroundColor: "white",
                     },
-                    "&:hover fieldset": {
-                      borderColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
                     },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "white",
+                  }}
+                />
+              </Grid>
+
+              {/* Travelers Field */}
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  label="Number of Tickets"
+                  type="number"
+                  variant="outlined"
+                  fullWidth
+                  value={searchData.numberOfTickets}
+                  onChange={(e) =>
+                    setSearchData({
+                      ...searchData,
+                      numberOfTickets: e.target.value,
+                    })
+                  }
+                  sx={{
+                    backgroundColor: "white",
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 3,
+                      "& fieldset": {
+                        borderColor: "lightgray",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#001e3c",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#001e3c",
+                      },
                     },
-                  },
-                },
-              }}
-            />
-          </Box>
+                  }}
+                />
+              </Grid>
+
+              {/* Search Button */}
+              <Grid item xs={12} sm={2}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                  sx={{
+                    backgroundColor: "#ff5a5f",
+                    color: "white",
+                    padding: "15px 0",
+                    borderRadius: 3,
+                    "&:hover": {
+                      backgroundColor: "#e0484d",
+                    },
+                  }}
+                  startIcon={<SearchIcon />}
+                >
+                  Search
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
         </Box>
-
-        <Button
-          variant="contained"
-          sx={{ marginTop: 2 }}
-          startIcon={<SearchIcon />}
-        >
-          Search
-        </Button>
       </Box>
     </Box>
   );
